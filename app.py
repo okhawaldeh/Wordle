0c08d67eda97e62628110cf4d94ccc5cb116c6ad
okhawaldeh <okhawaldeh2002@gmail.com>
2022-10-23 02:17:02 -0400

    again

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         definition = definition_finder()
-        return render_template('index.html', definition = definition)
+        return render_template('index.html', definition1 = definition)
     return render_template('index.html')
 if __name__ == "__main__":
     app.run(debug=True)
