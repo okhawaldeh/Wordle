baebd7f33d467cba3663b73c2efc726111783710
Omar <okhawaldeh2002@gmail.com>
2023-06-18 15:34:20 -0400

    fixed functionality

--- definition.py
+++ definition.py
@@ -8,70 +8,53 @@ from bs4 import BeautifulSoup
 #print(soup.prettify())
 
 def definition_finder():
+    
+    ####################finding the wordle####################
     url_wordle = "https://www.tomsguide.com/news/what-is-todays-wordle-answer"
+    # url_wordle = "https://word.tips/todays-wordle-answer/"
 
     #calls the requests lib
     resultW = requests.get(url_wordle)
 
     #make sure wesbite is accessible, should return 200
-    #print(result.status_code)
+    # print(resultW.status_code) 
 
     #creates a var of all the content of the website
     srcW = resultW.content
     #print(src)
 
     #create a soup object of the source
-    soupW = BeautifulSoup(srcW,'lxml')
+    soupW = BeautifulSoup(srcW,'html.parser')
 
-    #takes out the links and prints them
-    tagsW = soupW.findAll('strong')[1]
-    #print(tagsW.string)
+    #find the strong tags and take the last one in the list
+    tags = soupW.find_all('strong')
+    tag = tags[len(tags)-1]
+    
+    #extract the wordle of the tag
+    wordle = ""
+    for i in tag:
+        if i.isupper():
+            wordle = wordle + i
 
-    #words =[] #list of the words inside tag strong
-    wordle = tagsW.string #the wordle of the day
+    # print(wordle)
 
-    #appends the words inside the tag
-    #for tag in tagsW.string.split():
-    #    words.append(tag)
-
-    #finds the wordle by looking after string "is"
-    #for i in range(len(words)):
-    #    if(words[i] == "is"):
-    #        wordle = words[i + 1]
-            
-    #print(wordle)
-
-    url_def = "https://www.vocabulary.com/dictionary/" + wordle
+    ####################finding the definition####################
+    url_def = "https://www.merriam-webster.com/dictionary/" + wordle
     resultD = requests.get(url_def)
-    #print(resultD.status_code)
+    # print(resultD.status_code)
 
     srcD = resultD.content
-    soupD = BeautifulSoup(srcD,'lxml')
+    soupD = BeautifulSoup(srcD,'html.parser')
 
-    #finds the specific line using its tag
-    tagD = soupD.find_all('div', attrs={'class': 'definition'})
-        
-    lines =[] #list of the words inside tag strong
-    definition = "" #definition string
+    # finds the specific line using its tag
+    tagD = soupD.find("span", {"class": "dtText"}).get_text()
 
-    #appends the words inside the tag
-    for tag in tagD[0].get_text().split():
-        lines.append(tag)
-
-    #putting it together in a string
-    for i in range(len(lines)):
-        if i == 0:
-            definition += lines[i].title()
-            definition += ":"
-        else:
-            definition = definition + " " + lines[i]
-    definition+= "."
-
+    # taking what we need from the string
+    definition = tagD[2:len(tagD)]
+    # print(definition)
     
     return(definition)
 
-def main():
-    print(definition_finder())
-    
 if __name__ == "__main__":
-    main()
+    print(definition_finder())
+    # definition_finder()
